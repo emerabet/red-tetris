10348b495b925f10cc659dca8354cdecb70fb122
Furyou81 <lfujimot@student.42.fr>
2019-04-09 15:50:05 +0700

    test sagas

--- client/src/containers/GamePage/__TEST__/GamePage.test.tsx
+++ client/src/containers/GamePage/__TEST__/GamePage.test.tsx
@@ -68,43 +68,6 @@ it('renders correctly with defaults', () => {
   expect(gamePage).toMatchSnapshot();
 });
 
-// it('Test click event', () => {
-//   const mockCallBack = jest.fn();
-
-//   const button = shallow((<GamePage
-//     state={{
-//       grid: [[]],
-//       level: 0,
-//       pieces: '',
-//       score: 0,
-//       spectre: '0000000000',
-//     }}
-//     nagivation={null}
-//     history={null}
-//     startGame={mockCallBack}
-//     endGame={() => { }}
-//     resetGame={() => { }}
-//     started = {false}
-//     spectres={
-//       [{
-//         id: 'oponent 1',
-//         spectre: '0123456789',
-//         username: 'aaaaa',
-//       }]
-//     }
-//     count={2}
-//     username="test"
-//     action="joined"
-//     updatePlayers={() => {}}
-//     />));
-//   button.find('.plus').simulate('click');
-//   expect(mockCallBack.mock.calls.length).toEqual(1);
-// });
-
-// import React from 'react';
-// import mock-io, {serverSocket, cleanUp } from 'socket.io-client';
-// import {render} from 'react-testing-library';
-// import Chat from './chat';
 test('App should get messages', () => {
   // first render the app
   const utils = render(<GamePage
@@ -132,7 +95,7 @@ test('App should get messages', () => {
     username="test"
     action="joined"
     updatePlayers={() => {}}
-    />)
+    />);
 
   io.connect().on('state', () => {test: 'ettet'});
   
