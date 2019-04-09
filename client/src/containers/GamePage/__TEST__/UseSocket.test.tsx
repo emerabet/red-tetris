37bd7bdc48693b217be9eb7de546157fc0cda30c
eric <eric@imac.local>
2019-04-09 23:31:18 +0200

    test(useSocke): expect added

--- client/src/containers/GamePage/__TEST__/UseSocket.test.tsx
+++ client/src/containers/GamePage/__TEST__/UseSocket.test.tsx
@@ -11,12 +11,13 @@ configure({adapter: new Adapter()});
 it('test receive socket', (done: any) => {
     let EVENTS:Map<string, Function[]> = new Map<string, Function[]>();
     function emit(event, ...args) {
-    EVENTS[event].forEach((func: Function) => func !== undefined && func(...args));
+      EVENTS[event].forEach((func: Function) => {
+        func(...args);
+      });
     }
-    const serverSocket = { emit };
-    const mockCallBack1 = jest.fn();
-    const mockCallBack2 = jest.fn();
-    const mockCallBack3 = jest.fn();
+    const mockCallBack1 = jest.fn().mockImplementation();
+    const mockCallBack2 = jest.fn().mockImplementation();
+    const mockCallBack3 = jest.fn().mockImplementation();
     const socket = {
         on(event, func) {
          if (EVENTS[event]) {
@@ -26,20 +27,22 @@ it('test receive socket', (done: any) => {
         },
         emit
        };
-       
+
     const wrapper = mount(<Provider store={store}><UseSocket
         socket={socket}
         updateState={mockCallBack1}
         updateSpectre={mockCallBack2}
         updatePlayers={mockCallBack3}
       /></Provider>);
-      socket.on('state', wrapper.props()['updateState']);
-      socket.on('spectre', wrapper.props()['updateSpectre']);
-      socket.on('update_game_state', wrapper.props()['updatePlayers']);      
+      console.log('wrapper props:', wrapper.props().children.props);
+      socket.on('state', wrapper.props().children.props.updateState);
+      socket.on('spectre', wrapper.props().children.props.updateSpectre);
+      socket.on('update_game_state', wrapper.props().children.props.updatePlayers);
       socket.emit('state');
       socket.emit('spectre');
       socket.emit('update_game_state');
     setTimeout(()=>{ done(); }, 1500);
-    console.log(mockCallBack1.mock.calls.length, wrapper.props());
-    // expect(mockCallBack1.mock.calls.length).toEqual(1);
+    expect(wrapper.props().children.props.updateState).toHaveBeenCalledTimes(1);
+    expect(wrapper.props().children.props.updateSpectre).toHaveBeenCalledTimes(1);
+    expect(wrapper.props().children.props.updatePlayers).toHaveBeenCalledTimes(1);
 });
